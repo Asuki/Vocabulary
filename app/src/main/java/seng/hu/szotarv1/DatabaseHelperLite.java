7caa6869a9b83fd6c6cc898fd45e2a36985a078a
Asuki <phraseng@gmail.com>
2020-02-09 12:07:00 +0100

    Adding word table Modifying lesson table Adding db functions:  - adding word, lesson, book and language  - getting words

--- app/src/main/java/seng/hu/szotarv1/DatabaseHelperLite.java
+++ app/src/main/java/seng/hu/szotarv1/DatabaseHelperLite.java
@@ -2,6 +2,7 @@ package seng.hu.szotarv1;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -26,6 +27,7 @@ public class DatabaseHelperLite extends SQLiteOpenHelper {
     private static final String DELET_ALL_FROM_TABLE = "delete from ";
     private static final String SELECT_ALL_ROW_FROM_TABLE = "select * from ";
 
+    // Storing languages.
     public static final String LANGUAGE_TABLE = "languages";
     private static final String ID = "id";
     private static final String LANGUAGE_NAME = "language_name";
@@ -33,79 +35,214 @@ public class DatabaseHelperLite extends SQLiteOpenHelper {
     private static final String CREATE_LANGUAGE_NAME = LANGUAGE_NAME + STRING_TYPE_50;
     private static final String CREATE_LANGUAGE_TABLE = CREATE_TABLE + LANGUAGE_TABLE + " (" +
             CREATE_LANGUAGE_ID + COMMA_SEPARATOR +
-            CREATE_LANGUAGE_NAME +  ");";
+            CREATE_LANGUAGE_NAME + UNIQUE_MODIFIER + ");";
 
+    // Storing books.
     public static final String BOOK_TABLE = "books";
     private static final String BOOK_TITLE = "book_title";
-    private static final String FIRST_LANGUAGE_ID_F = "language_id_1";
-    private static final String SECOND_LANGUAGE_ID_F = "language_id_2";
+    private static final String FIRST_LANGUAGE = "first_language_name";
+    private static final String SECOND_LANGUAGE = "second_language_name";
     private static final String CREATE_BOOK_ID = ID + ID_TYPE;
     private static final String CREATE_BOOK_TITLE = BOOK_TITLE + STRING_TYPE_50;
-    private static final String CREATE_FIRST_LANGUAGE_ID_F = FIRST_LANGUAGE_ID_F + ID_TYPE;
-    private static final String CREATE_SECOND_LANGUAGE_ID_F = SECOND_LANGUAGE_ID_F + ID_TYPE;
+    private static final String CREATE_FIRST_LANGUAGE_ID_F = FIRST_LANGUAGE + STRING_TYPE_50;
+    private static final String CREATE_SECOND_LANGUAGE_ID_F = SECOND_LANGUAGE + STRING_TYPE_50;
     private static final String CREATE_BOOK_TABLE = CREATE_TABLE + BOOK_TABLE + " (" +
             CREATE_BOOK_ID + COMMA_SEPARATOR +
             CREATE_BOOK_TITLE + COMMA_SEPARATOR +
             CREATE_FIRST_LANGUAGE_ID_F + COMMA_SEPARATOR +
             CREATE_SECOND_LANGUAGE_ID_F + ");";
 
+    // Storing lessons of books.
     public static final String LESSON_TABLE = "lesson";
+    private static final String LESSON_NAME = "name";
     private static final String BOOK_ID_F = "book_id";
-    private static final String FIRST_LANGUAGE_WORD = "first_language_word";
-    private static final String SECOND_LANGUAGE_MEANING = "second_language_meaning";
-    private static final String KNOWLEDGE_PERCENT = "knowledge_percent";
-    private static final String EXAMPLE_SENTENCE = "example_sentence";
     private static final String CREATE_LESSON_ID = ID + ID_TYPE;
+    private static final String CREATE_LESSON_NAME = LESSON_NAME + STRING_TYPE_50;
     private static final String CREATE_BOOK_ID_F = BOOK_ID_F + ID_TYPE;
-    private static final String CREATE_FIRST_LANGUAGE_WORD = FIRST_LANGUAGE_WORD + STRING_TYPE_100;
-    private static final String CREATE_SECOND_LANGUAGE_MEANING = SECOND_LANGUAGE_MEANING + STRING_TYPE_200;
-    private static final String CREATE_KNOWLEDGE_PERCENT = KNOWLEDGE_PERCENT + INT_TYPE;
-    private static final String CREATE_EXAMPLE_SENTENCE = EXAMPLE_SENTENCE + STRING_TYPE_200;
     private static final String CREATE_LESSON_TABLE = CREATE_TABLE + LESSON_TABLE + " (" +
             CREATE_LESSON_ID + COMMA_SEPARATOR +
-            CREATE_BOOK_ID_F + COMMA_SEPARATOR +
-            CREATE_FIRST_LANGUAGE_WORD + COMMA_SEPARATOR +
-            CREATE_SECOND_LANGUAGE_MEANING + COMMA_SEPARATOR +
-            CREATE_KNOWLEDGE_PERCENT + COMMA_SEPARATOR +
-            CREATE_EXAMPLE_SENTENCE + ");";
+            CREATE_LESSON_NAME + COMMA_SEPARATOR +
+            CREATE_BOOK_ID_F + ");";
+
+    public static final String WORD_TABLE = "words";
+    private static final String WORD  = "word";
+    private static final String LANGUAGE_NAME_F  = "language_name";
+    private static final String MEANING = "meaning";
+    private static final String LESSON_ID_F = "lesson_id";
+    private static final String KNOWLEDGE = "knowledge";
+    private static final String EXAMPLE_SENTENCE = "example_sentence";
+    private static final String CREATE_WORD_ID = ID + ID_TYPE;
+    private static final String CREATE_WORD = WORD + STRING_TYPE_100;
+    private static final String CREATE_LANGUAGE_NAME_F = LANGUAGE_NAME_F + STRING_TYPE_50;
+    private static final String CREATE_MEANING = MEANING + STRING_TYPE_200;
+    private static final String CREATE_LESSON_ID_F = LESSON_ID_F + ID_TYPE;
+    private static final String CREATE_KNOWLEDGE = KNOWLEDGE + INT_TYPE;
+    private static final String CREATE_EXAMPLE_SENTENCE = EXAMPLE_SENTENCE + STRING_TYPE_200;
+    private static final String CREATE_WORDS_TABLE = CREATE_TABLE + WORD_TABLE + " (" +
+            CREATE_WORD_ID + COMMA_SEPARATOR +
+            CREATE_WORD + COMMA_SEPARATOR +
+            CREATE_LANGUAGE_NAME_F + COMMA_SEPARATOR +
+            CREATE_MEANING + COMMA_SEPARATOR +
+            CREATE_LESSON_ID_F + COMMA_SEPARATOR +
+            CREATE_KNOWLEDGE + COMMA_SEPARATOR +
+            CREATE_EXAMPLE_SENTENCE + " );";
 
 
     public DatabaseHelperLite(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Creating the database and its tables
+     * @param sqLiteDatabase
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_LANGUAGE_TABLE);
         sqLiteDatabase.execSQL(CREATE_BOOK_TABLE);
         sqLiteDatabase.execSQL(CREATE_LESSON_TABLE);
+        sqLiteDatabase.execSQL(WORD_TABLE);
         Log.d(TAG, "onCreate: create " + LANGUAGE_TABLE + " table + " + CREATE_LANGUAGE_TABLE);
         Log.d(TAG, "onCreate: create " + BOOK_TABLE + " table + " + CREATE_BOOK_TABLE);
         Log.d(TAG, "onCreate: create " + LESSON_TABLE + " table + " + CREATE_LESSON_TABLE);
+        Log.d(TAG, "onCreate: create " + WORD_TABLE + " table + " + CREATE_WORDS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
     }
 
-    public boolean addWord(String title, String firstLanguageWord, String meaning, String example){
-        // ToDO: check this
+    /**
+     * Adding new language
+     * @param language: the language what you want to add
+     * @return
+     */
+    private boolean addLanguage(String language){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(LANGUAGE_NAME, language);
+
+        long result = db.insert(LANGUAGE_TABLE, null, contentValues);
+        if(-1 == result)
+            return false;
+        return true;
+    }
+
+    /**
+     * Add new book. It also add the languages if them not exists
+     * @param title The title of the book
+     * @param languageFirst The first language. Mainly the language of the book
+     * @param languageSecond The second language. Mainly the users mother language
+     * @return True: if the new value have been inserted. False others.
+     */
+    public boolean addBook(String title, String languageFirst, String languageSecond){
         SQLiteDatabase db = getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(BOOK_TITLE, title);
-        contentValues.put(FIRST_LANGUAGE_WORD, firstLanguageWord);
-        contentValues.put(SECOND_LANGUAGE_MEANING, meaning);
-        contentValues.put(KNOWLEDGE_PERCENT, 0);
-        contentValues.put(EXAMPLE_SENTENCE, example);
+        contentValues.put(FIRST_LANGUAGE, languageFirst);
+        contentValues.put(SECOND_LANGUAGE, languageSecond);
 
-        Log.d(TAG, "addWord: {title: " + title + ", word: " +
-                firstLanguageWord + ", meaning: " + meaning +
-                ", example: " + example);
+        // Trying to add the languages to languages table
+        addLanguage(languageFirst);
+        addLanguage(languageSecond);
+
+        long result = db.insert(BOOK_TABLE, null, contentValues);
+        if (-1 == result){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Adding new lesson to a book
+     * @param name Name of the lesson
+     * @param bookId ID of the book
+     * @return true if the adding was successful false otherwise.
+     */
+    public boolean addLesson(String name, int bookId){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(LESSON_NAME, name);
+        contentValues.put(BOOK_ID_F, bookId);
 
         long result = db.insert(LESSON_TABLE, null, contentValues);
         if (-1 == result)
-            return  false;
-        return  true;
+            return false;
+        return true;
     }
+
+    /**
+     * Adding new word for a lesson.
+     * @param word The new word, what we want to ad.
+     * @param meaning The meanings of the word.
+     * @param example An example sentence what describes the word.
+     * @param lessonId The ID of the lesson where you want to add the word.
+     * @return True if the inserting was successful false otherwise.
+     */
+    public boolean addWord(String word, String meaning, String example, Integer lessonId, String languageName){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(WORD, word);
+        contentValues.put(MEANING, meaning);
+        contentValues.put(EXAMPLE_SENTENCE, example);
+        contentValues.put(LESSON_ID_F, lessonId);
+        contentValues.put(KNOWLEDGE, 0);
+        contentValues.put(LANGUAGE_NAME_F, languageName);
+
+        long result = db.insert(WORD_TABLE, null, contentValues);
+        if (result == -1)
+            return false;
+        return true;
+    }
+
+    /**
+     * Getting all words of a language.
+     * @param language The languages what's words we want to get.
+     * @return The cursor of the words.
+     */
+    public Cursor getAllWords(String language) {
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "select " + WORD + ", " + MEANING +
+                " from " + WORD_TABLE +
+                " where " + LANGUAGE_NAME_F + " = " + language;
+        Log.d(TAG, "getAllWords: query: " + query);
+        Cursor data = db.rawQuery(query, null);
+        Log.d(TAG, "getAllWords: query run successfully");
+        return data;
+    }
+
+    /**
+     * Getting all words of a lesson.
+     * @param lessonId The ID of a lesson what's words we want to get.
+     * @return The cursor of the words.
+     */
+    public Cursor getAllWordsOfLesson(Integer lessonId) {
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "select " + WORD + ", " + MEANING +
+                " from " + WORD_TABLE +
+                " where " + LESSON_ID_F + " = '" + lessonId + "'";
+        Log.d(TAG, "getAllWordsOfLesson: query: " + query);
+        Cursor data = db.rawQuery(query, null);
+        Log.d(TAG, "getAllWordsOfLesson: query run successfully");
+        return data;
+    }
+
+    /**
+     * Getting all words of a book.
+     * @param bookId The ID of the book what's words we want to get.
+     * @return The cursor of the words.
+     */
+    public Cursor getAllWordsOfBook(Integer bookId) {
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "select " + WORD + ", " + MEANING +
+                " from " + WORD_TABLE + " as w " +
+                " join " + LESSON_TABLE + " as l on w." + LESSON_ID_F + " = l." + ID +
+                " where l." + BOOK_ID_F + " = '" + bookId + "'";
+        Log.d(TAG, "getAllWordsOfBook: query: " + query);
+        Cursor data = db.rawQuery(query, null);
+        Log.d(TAG, "getAllWordsOfBook: query run successfully");
+        return data;
+    }
+
 
 }
